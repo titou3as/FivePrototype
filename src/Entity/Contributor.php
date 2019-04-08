837cdbaecc1af44168fc9caffe88d310795e40a7
khaoula-abaidi <abaidik@gmail.com>
2019-04-08 15:02:22 +0200

    Adding decisions command

--- src/Entity/Contributor.php
+++ src/Entity/Contributor.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ContributorRepository")
@@ -20,6 +21,12 @@ class Contributor
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(
+     *     min = 5,
+     *      max = 20,
+     *      minMessage = "Le login doit être composé au moins par {{ limit }} caractères",
+     *      maxMessage = "Le login ne doit pas dépassé {{ limit }} caractères"
+     * )
      */
     private $login;
 
